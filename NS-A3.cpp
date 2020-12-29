8d8444c8fc7f7093eadf34ce39e9b1ee6f721c8c
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2020-12-29 19:35:45 +0500

    Square And Cube
    Finding Square and cube of a given Number

---
+++ NS-A3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+
+int main() {
+	int x, square, cube;
+	cout << "******Ahsan Tariq******"<<endl;
+	cout<<"Enter a Number"<<endl;
+	cin >> x;
+	square = x * x;
+	cube = x * x * x;
+	cout << "The square of the number is  " << square<<endl;
+	cout << "The Cube of the number is    " << cube<<endl;
+	return 0;
+
+
+}
