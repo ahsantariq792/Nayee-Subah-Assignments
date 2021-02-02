1deb3d6f572459875e2e71b508aa0ee08834b6f4
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-02-02 23:55:00 +0500

    Table

---
+++ NSF-A8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	for (int i = 10; i >-1; i--) {
+		cout << i << endl;
+	}
+	int i = 10;
+	while (i > -1) {
+		cout << i << endl;
+		i--;
+	}
+}
+
